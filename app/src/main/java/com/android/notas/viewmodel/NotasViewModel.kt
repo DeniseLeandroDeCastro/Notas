457fb3569da5f2590898654a10d1679af540c307
DeniseLeandroDeCastro <dmaria.natal@gmail.com>
2022-07-09 20:27:30 -0300

    Implementar a NotasViewModel

--- app/src/main/java/com/android/notas/viewmodel/NotasViewModel.kt
+++ app/src/main/java/com/android/notas/viewmodel/NotasViewModel.kt
@@ -1,5 +1,19 @@
 package com.android.notas.viewmodel
 
-class NotasViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.android.notas.data.GestorDeNotas
+import com.android.notas.data.Nota
 
+class NotasViewModel: ViewModel() {
+    val gestorDeNotas = GestorDeNotas()
+
+    private val mNotas = MutableLiveData<List<Nota>>()
+
+    fun getNotas(): MutableLiveData<List<Nota>> = mNotas
+
+    fun carregarNotas() {
+        val tmp = gestorDeNotas.getNotas()
+        mNotas.postValue(tmp)
+    }
 }
